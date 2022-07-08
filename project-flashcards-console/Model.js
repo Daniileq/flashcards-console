fd467a104a3c479da21619688163cf010865bbf8
Daniil <ledyaev-danil@mail.ru>
2022-07-08 12:09:59 +0300

    getData1

--- project-flashcards-console/Model.js
+++ project-flashcards-console/Model.js
@@ -1,7 +1,25 @@
-class Model {
-  constructor() {
+// const { rejects } = require("assert");
+const fs = require("fs");
+// const { resolve } = require("path");
 
+class Model {
+  constructor(question, answer) {
+    this.question = question;
+    this.answer = answer;
+  }
+  getData() {
+    const readDir = new Promise((resolve, rejects) => {
+      fs.readdir("./topics", (err, data) => {
+        if (err) return rejects(err);
+        return resolve(data);
+      });
+    });
+    
+
+    readDir.then((data) => console.log(data));
   }
 }
+const r = new Model();
+r.getData();
 
-module.exports = Model
+module.exports = Model;

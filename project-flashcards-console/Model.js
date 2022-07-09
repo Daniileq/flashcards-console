2ca9c318fba5c420487bedc6851512cabe60dd4b
Vitaliy <vetal9305@gmail.com>
2022-07-09 15:35:56 +0300

    Запустил игру. Есть один баг. Буду исправлять.

--- project-flashcards-console/Model.js
+++ project-flashcards-console/Model.js
@@ -1,5 +1,10 @@
 const fs = require("fs");
+const readline = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
+let arrRes;
 
 class Questansw {
   constructor(question, answer) {
@@ -33,18 +38,40 @@ class Model {
       .then((promises) => Promise.all(promises))
       .then((er) =>
         er.map((el) => {
-          return el.match(/.+\n/gi);
+          return el.match(/.+/gi);
         })
       )
-      .then((arr) => arr.flat(1).filter((el) => el !== "---\n"))
       .then((arr) =>
-        arr.map((el, i, arr) => {
+        arr.flat(1).map((el, i, arr) => {
           if (i % 2 != 0) i++;
           return new Questansw(arr[i], arr[i + 1]);
         })
       )
-      .then(console.log);
+      .then((e) => run(e));
   }
 }
+
+const r = new Model();
+arrRes = r.getData();
+let i = 0;
+
+const run = (arrRes) => {
+  console.log(`\n${arrRes[i].question}\n`);
+  readline.question(`Напишите ответ - `, (userCount) => {
+    if (arrRes.length - 1 === i) {
+      readline.close;
+      console.log("игра завершена!!!");
+    }
+    if (userCount.toLowerCase() === arrRes[i].answer.toLowerCase()) {
+      console.log("\n!!!Верно!!!");
+      i++;
+      run(arrRes);
+    } else {
+      console.log("\nНеверно(((");
+      i++;
+      run(arrRes);
+    }
+  });
+};
 
 module.exports = Model;

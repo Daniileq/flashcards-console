7ccf4a21abfbcf7afe9e016605852a96a6190005
Vitaliy <vetal9305@gmail.com>
2022-07-10 13:07:27 +0300

    Переделал Model что бы у него было два независимых метода на предоставление меню и списка вопросов ответов.

--- project-flashcards-console/Model.js
+++ project-flashcards-console/Model.js
@@ -1,10 +1,9 @@
 const fs = require("fs");
-const readline = require("readline").createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+// const readline = require("readline").createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
 
-let arrRes = [];
 let i = 0;
 
 class Questansw {
@@ -15,63 +14,66 @@ class Questansw {
 }
 
 class Model {
-  getData() {
-    function readDir() {
-      return new Promise((resolve, rejects) => {
-        fs.readdir("./topics/", (err, data) => {
-          if (err) return rejects(err);
-          return resolve(data);
-        });
+  getMenu() {
+    return new Promise((resolve, rejects) => {
+      fs.readdir("./topics/", (err, data) => {
+        if (err) return rejects(err);
+        return resolve(data);
       });
-    }
-
-    function readFile(path, code) {
-      return new Promise((resolve, rejects) => {
-        fs.readFile(`./topics/${path}`, code, (err, file) => {
-          if (err) return rejects(err);
-          return resolve(file);
-        });
-      });
-    }
-
-    readDir()
-      .then((fileNames) => fileNames.map((file) => readFile(file, "utf-8")))
-      .then((promises) => Promise.all(promises))
-      .then((er) =>
-        er.map((el) => {
-          return el.match(/.+/gi);
-        })
-      )
-      .then((arr) =>
-        arr.flat(1).forEach((el, i, arr) => {
-          if (i % 2 === 0) arrRes.push(new Questansw(arr[i], arr[i + 1]));
-        })
-      )
-      .then((e) => run(arrRes));
+    });
   }
+  getQwest(path) {
+    let arrRes = [];
+    return new Promise((resolve, rejects) => {
+      fs.readFile(`./topics/${path}`, "utf8", (err, file) => {
+        if (err) return rejects(err);
+        let text = file.split("\n").filter((el) => el != "");
+        text.forEach((el, i, arr) => {
+          if (i % 2 === 0) arrRes.push(new Questansw(arr[i], arr[i + 1]));
+        });
+
+        return resolve(arrRes);
+      });
+    });
+  }
+
+  // readDir()
+  //   .then((fileNames) => fileNames.map((file) => readFile(file, "utf-8")))
+  //   .then((promises) => Promise.all(promises))
+  //   .then((er) =>
+  //     er.map((el) => {
+  //       return el.match(/.+/gi);
+  //     })
+  //   )
+  //   .then((arr) =>
+  //     arr.flat(1).forEach((el, i, arr) => {
+  //       if (i % 2 === 0) arrRes.push(new Questansw(arr[i], arr[i + 1]));
+  //     })
+  //   )
+  //   .then((e) => run(arrRes));
 }
 
-const runner = new Model();
-runner.getData();
+// const runner = new Model();
+// runner.getQwest("1.Bird.txt").then(console.log);
 
-const run = (arrRes) => {
-  if (i < arrRes.length - 1) {
-    console.log(`\n${arrRes[i].question}\n`);
-    readline.question(`Напишите ответ - `, (userCount) => {
-      if (userCount.toLowerCase() === arrRes[i].answer.toLowerCase()) {
-        console.log("\n!!!Верно!!!");
-        i++;
-        run(arrRes);
-      } else if (userCount.toLowerCase() !== arrRes[i].answer.toLowerCase()) {
-        console.log("\nНеверно(((");
-        i++;
-        run(arrRes);
-      }
-    });
-  } else {
-    console.log("\n!!!Игра завершена!!!");
-    readline.close;
-  }
-};
+// const run = (arrRes) => {
+//   if (i < arrRes.length - 1) {
+//     console.log(`\n${arrRes[i].question}\n`);
+//     readline.question(`Напишите ответ - `, (userCount) => {
+//       if (userCount.toLowerCase() === arrRes[i].answer.toLowerCase()) {
+//         console.log("\n!!!Верно!!!");
+//         i++;
+//         run(arrRes);
+//       } else if (userCount.toLowerCase() !== arrRes[i].answer.toLowerCase()) {
+//         console.log("\nНеверно(((");
+//         i++;
+//         run(arrRes);
+//       }
+//     });
+//   } else {
+//     console.log("\n!!!Игра завершена!!!");
+//     readline.close;
+//   }
+// };
 
 module.exports = Model;

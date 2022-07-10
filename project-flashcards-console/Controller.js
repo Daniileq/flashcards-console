eaabb01e16bf83ba934b013e5fa20bb53599d37b
Vitaliy <vetal9305@gmail.com>
2022-07-10 14:01:46 +0300

    Написал метод для обработки вопросов

--- project-flashcards-console/Controller.js
+++ project-flashcards-console/Controller.js
@@ -10,17 +10,27 @@ class Controller {
   async init() {
     const topics = await this.model.getMenu();
     this.topics = topics;
-    run();
+    this.run();
   }
 
-  run() {
-    
+  async run() {
+    const menuNum = await this.view.showMenu();
+    const topic = this.topics[menuNum - 1];
+    const questList = await this.model.getQwest(topic);
+    for (let i = 0; i < questList; i++) {
+      const answer = await this.view.showQuest(questList[i].question);
+      const resultUser =
+        questList[i].answer.toLowerCase() === answer.toLowerCase();
+      await this.view.showResult(resultUser);
+    }
+    this.run();
+
     // Просим экземпляр класса модели прочитать папку со всеми темами и составить меню.
     // Попутно передаем метод контроллера this.printTopicsController,
     // так как нам нужно отправить сформинованное меню на вывод в экземпляр класса view
     // после того, как завершится асинхронная операция чтения папки
     // Здесь this.printTopicsController — является callback'ом
-    this.model.getMenu(this.printTopicsController);
+    // this.model.getMenu(this.printTopicsController);
   }
 }
 
